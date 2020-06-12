88a2121f8a30ff9ea6750235364eab2583db7982
Lizzie Prader <lizzie@moddable.tech>
2020-06-11 22:45:58 -0700

    network test

---
+++ runmod-network/main.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016-2020 Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import LoadMod from "loadmod";
+import WiFi from "wifi";
+import Net from "net";
+import Preference from "preference";
+
+export default function () {
+	if (!LoadMod.has("check") || !LoadMod.has("example"))
+		return trace("Host installed. Ready for mods.\n");
+
+	(LoadMod.load("check"))();
+
+	WiFi.mode = 1;
+
+	let ssid = Preference.get("wifi", "ssid") 
+	let password = Preference.get("wifi", "password") 
+	let monitor = new WiFi({ssid, password}, function(msg, code) {
+	   switch (msg) {
+		   case "gotIP":
+				trace(`IP address ${Net.get("IP")}\n`);
+				monitor.close();
+
+				LoadMod.load("example");
+				break;
+
+			case "connect":
+				trace(`Wi-Fi connected to "${Net.get("SSID")}"\n`);
+				break;
+
+			case "disconnect":
+				trace((-1 === code) ? "Wi-Fi password rejected\n" : "Wi-Fi disconnected\n");
+				break;
+		}
+	});
+}
